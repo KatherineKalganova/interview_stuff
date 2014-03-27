65560a8e28b844ddd6496e69e8726a8ec70cf8d1
KatherineKalganova <kalganovaaaaa@yandex.ru>
2014-03-27 02:12:46 -0700

    Update pow.rb

--- programming/pow.rb
+++ programming/pow.rb
@@ -2,9 +2,21 @@
 
 def pow(x, n)
 	res = 0.0
-
-	#TODO change this code
-	res = x**n
 	
-	return res
+	res = 1.0
+n_abs = n.abs
+if (n_abs.to_i - n_abs)==0 then
+ while n_abs>0 do
+ if n_abs%2==0 then
+    n_abs = n_abs/2
+    x = x*x
+   else
+    n_abs = n_abs-1
+    res = res*x
+	
+if n>=0 then
+return res
+else
+return 1/res
+	
 end
